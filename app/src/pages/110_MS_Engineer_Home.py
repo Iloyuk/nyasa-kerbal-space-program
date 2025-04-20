ec050e9a9ffda050ad59f40ea7ff8b7e11967e0f
Jason Sima <jasonsima2006@gmail.com>
2025-04-19 21:17:53 -0700

    carl was fixed

--- app/src/pages/110_MS_Engineer_Home.py
+++ app/src/pages/110_MS_Engineer_Home.py
@@ -1,10 +1,11 @@
 import logging
+
 logger = logging.getLogger(__name__)
 import requests
 import streamlit as st
 from modules.nav import SideBarLinks
 
-st.set_page_config(layout = 'wide')
+st.set_page_config(layout='wide')
 
 SideBarLinks()
 
@@ -16,9 +17,9 @@ main = st.empty()
 data = {}
 with st.form("help"):
     title = st.text_input("Mission Name")
-    data = requests.get(f'http://api:4000/missions/name/"{(title)}"').json()
-    if data == []:
+    if '/' in title or not data:
         data = requests.get('http://api:4000/missions/objective').json()
+    else:
+        data = requests.get(f'http://api:4000/missions/name/"{title}"').json()
     st.form_submit_button('Update')
     st.dataframe(data)
-    
