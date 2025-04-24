aca6ac0674cd6bd5444e7bc6d6ab924d6c028c2f
Jason Sima <jasonsima2006@gmail.com>
2025-04-23 21:11:21 -0700

    date fix

--- app/src/pages/130_Astrologist_Home.py
+++ app/src/pages/130_Astrologist_Home.py
@@ -1,10 +1,11 @@
 import logging
+
 logger = logging.getLogger(__name__)
 import requests
 import streamlit as st
 from modules.nav import SideBarLinks
 
-st.set_page_config(layout = 'wide')
+st.set_page_config(layout='wide')
 
 SideBarLinks()
 
@@ -16,9 +17,9 @@ st.write('### What would you like to do today?')
 if st.button('View all constellations and their information',
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/131_Constellation_Info.py')
+    st.switch_page('pages/131_Constellation_Info.py')
 
 if st.button("See which constellation a certain star is in",
-              type='primary',
-              use_container_width=True):
-  st.switch_page('pages/132_Find_Constellation.py')
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/132_Find_Constellation.py')
