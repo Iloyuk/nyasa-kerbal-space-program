aef9534f05d083245be935dd2aa6adc379a526b5
Jason Sima <jasonsima2006@gmail.com>
2025-04-23 22:53:55 -0700

    about page

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -12,8 +12,17 @@ with st.container():
         The Kerbal Space Program database app was made by Jason, Davey, and Kylie. 
 
         It is a database app that keep tracks of galaxies, stars, star systems, and more, with the ability to update
-        it and view, made with the intent of easy control and idenfiable info. 
+        it and view, made with the intent of easy control and identifiable info. 
         """
             )
-    st.image("assets/faust.png", width=256, caption="hello! - davey")
-    st.image("assets/skye.png", width=256, caption="sammy killed my will to live - jason")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.image("assets/faust.png", width=270, caption="hello! - davey")
+
+    with col2:
+        st.image("assets/skye.png", width=256, caption="sammy killed my will to live - jason")
+
+    with col3:
+        st.image("assets/nug.png", width=272, caption="dino nuggiez are yummy - kylie")
